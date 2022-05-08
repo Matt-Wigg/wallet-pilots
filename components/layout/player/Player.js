bb5330ea50f493b3dbd04ed45c264ed530f6a5e2
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-05-07 17:28:35 -0700

    attempt at audio player css

--- components/layout/player/Player.js
+++ components/layout/player/Player.js
@@ -31,6 +31,7 @@ const AudioPlayer = () => {
   const nextSong = (e) => {
     if (currentTrack === tracks.length - 1) setCurrentTrack(0);
     else setCurrentTrack(currentTrack + 1);
+    e.preventDefault();
   };
 
   useEffect(() => {
@@ -63,49 +64,50 @@ const AudioPlayer = () => {
           <span className={isPlaying ? style.musicBars : style.paused} />
         </div>
         <div className={style.buttonContainer}>
-          <div className={style.buttonContainer}>
-            <button className={style.playerButton} onClick={restartAndPrevious}>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 24 24"
-                height="100%"
-                width="100%"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="m16 7-7 5 7 5zm-7 5V7H7v10h2z"></path>
-              </svg>
-            </button>
-            <button className={isPlaying ? style.isPlaying : style.playerButton } onClick={togglePlayPause}>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 24 24"
-                height="100%"
-                width="100%"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d={isPlaying ? "M8 7h3v10H8zm5 0h3v10h-3z" : "M7 6v12l10-6z"}
-                ></path>
-              </svg>
-            </button>
-            <button className={style.playerButton} onClick={nextSong}>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 24 24"
-                height="100%"
-                width="100%"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M7 7v10l7-5zm9 10V7h-2v10z"></path>
-              </svg>
-            </button>
-          </div>
+          <button className={style.playerButton} onClick={restartAndPrevious}>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="100%"
+              width="100%"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="m16 7-7 5 7 5zm-7 5V7H7v10h2z"></path>
+            </svg>
+          </button>
+          <button
+            className={isPlaying ? style.isPlaying : style.playerButton}
+            onClick={togglePlayPause}
+          >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="100%"
+              width="100%"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d={isPlaying ? "M8 7h3v10H8zm5 0h3v10h-3z" : "M7 6v12l10-6z"}
+              ></path>
+            </svg>
+          </button>
+          <button className={style.playerButton} onClick={(e) => nextSong(e)}>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="100%"
+              width="100%"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M7 7v10l7-5zm9 10V7h-2v10z"></path>
+            </svg>
+          </button>
         </div>
       </div>
     </div>
