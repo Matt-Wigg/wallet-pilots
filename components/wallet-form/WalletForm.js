0a727d05c0f3f03b5baf271ca82515a51357b0ee
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-05-23 17:44:55 -0700

    mvp opensea NFT name lookup

---
+++ components/wallet-form/WalletForm.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import styles from "./WalletForm.module.css";
+
+const WalletForm = () => {
+  const [openSeaData, setOpenSeaData] = useState([]);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { address: event.target.address.value };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/opensea-test";
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+    const dataStream = await response.json();
+    setOpenSeaData(dataStream.assets);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first">Search NFT names:</label>
+        <input
+          className={styles.input}
+          type="text"
+          id="address"
+          name="address"
+          placeholder="Enter wallet address..."
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div className={styles.openSeaData}>
+        {openSeaData && openSeaData.map((asset) => {
+          return <div key={asset.id}>{asset.name}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default WalletForm;
