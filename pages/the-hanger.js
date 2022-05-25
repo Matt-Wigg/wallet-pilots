b1aae92fcf46163b651c629f59cab213624df7f6
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-05-24 18:07:14 -0700

    auto search connected wallet

--- pages/the-hanger.js
+++ pages/the-hanger.js
@@ -1,18 +1,24 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useWeb3React } from "@web3-react/core";
 
 import WalletForm from "../components/wallet-form/WalletForm";
 
-const Hanger = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>The Hanger: Manage Assets</title>
-      <meta name="description" content="Wallet Pilot" key="title" />
-      <link rel="icon" href="/pilot.svg" />
-    </Head>
-    <h1>The Hanger:</h1>
-    <WalletForm />
-  </div>
-);
+const Hanger = (props) => {
+  const { account } = useWeb3React();
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        {console.log(props)}
+        <title>The Hanger: Manage Assets</title>
+        <meta name="description" content="Wallet Pilot" key="title" />
+        <link rel="icon" href="/pilot.svg" />
+      </Head>
+      <h1>The Hanger:</h1>
+      <WalletForm account={account} />
+    </div>
+  );
+};
 
 export default Hanger;
