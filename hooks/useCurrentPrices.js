19183cfb6d6098ca2d901cf6a805eb7a8eeb27e6
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-10-08 21:23:41 -0700

    converted to swr

--- hooks/useCurrentPrices.js
+++ hooks/useCurrentPrices.js
@@ -1,10 +1,11 @@
+import fetcher from './fetcher';
 import useSWR from 'swr';
 
 const useCurrentPrices = (id) => {
-  const { data, error } = useSWR(`/api/user/${id}`, fetcher);
+  const { data, error } = useSWR(`api/coinmarket/get-current-prices`, fetcher);
 
   return {
-    user: data,
+    prices: data,
     isLoading: !error && !data,
     isError: error,
   };
