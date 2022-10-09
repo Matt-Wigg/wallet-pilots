07915b347c309898b7765cc0acabda2fe6698b2e
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-10-09 03:50:02 -0700

    fix player css and letter spacing

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 const Home = () => {
   return (
     <div className={styles.main}>
       <Head>
         <title>Welcome To Wallet Pilot</title>
-        <meta name="description" content="Wallet Pilot" key="title" />
-        <link rel="icon" href="/pilot.svg" />
+        <meta name='description' content='Wallet Pilot' key='title' />
+        <link rel='icon' href='/pilot.svg' />
       </Head>
       <div className={styles.container}>
         <h1>Welcome to Wallet Pilot</h1>
@@ -16,19 +16,21 @@ const Home = () => {
           Wallet Pilot is an asset-governed approach to portfolio management.
         </p>
         <p>
-          Owning one of the 10,000 unique{" "}
-          <Link href="/pilots">
+          Owning one of the 10,000 unique{' '}
+          <Link href='/pilots'>
             <a>Pilot NTFS(...soon)</a>
           </Link>
           , living on the Ethereum blockchain, grants you access and development
-          dictatorship over{" "}
-          <Link href="/the-hanger">
+          dictatorship over{' '}
+          <Link href='/the-hanger'>
             <a>The Hanger(...soon)</a>
           </Link>
-          : A special portfolio management tool. Pilots also unlock other
-          things, depending on <i>when</i> and <i>where</i> they fly... There is
-          no Discord or Twitter, just a{" "}
-          <Link href="/map">
+          : A special portfolio management tool.
+        </p>{' '}
+        <p>
+          Pilots also unlock other things, depending on <i>when</i> and{' '}
+          <i>where</i> they fly... There is no Discord or Twitter, just a{' '}
+          <Link href='/map'>
             <a>Map(...soon)</a>
           </Link>
           .
